ee2eca433c45835d3bfb0564146946b922c5191b
Justin Venezuela <jven@mit.edu>
2012-11-14 10:26:17 -0500

    find_examples looks for numbers that can be expressed as the sum of 2 cubes in k ways

--- sum_of_cubes/find_examples.py
+++ sum_of_cubes/find_examples.py
@@ -9,12 +9,10 @@ def find_cube_sums_naive():
   d = {}
   s = 0
   while True:
-    for a in range(s):
+    for a in range(s / 2):
       b = s - a
       n = a * a * a + b * b * b
       if n in d:
-        if (a, b) in d[n] or (b, a) in d[n]:
-          break
         d[n].append((a, b))
         output = '%d' % n
         for l in d[n]:
@@ -24,5 +22,26 @@ def find_cube_sums_naive():
         d[n] = [(a, b)]
     s += 1
 
+def find_multiple_cube_sums_naive():
+  d = {}
+  s = 0
+  m = 2
+  while True:
+    for a in range(s / 2):
+      b = s - a
+      n = a * a * a + b * b * b
+      if n in d:
+        d[n].append((a, b))
+        if len(d[n]) >= m:
+          output = '%d expressions: %d' % (len(d[n]), n)
+          for l in d[n]:
+            output += ' = %d^3 + %d^3' % (l[0], l[1])
+          print output
+          m = len(d[n]) + 1
+      else:
+        d[n] = [(a, b)]
+    s += 1
+
 if __name__ == '__main__':
-  find_cube_sums_naive()
+  #find_cube_sums_naive()
+  find_multiple_cube_sums_naive()
